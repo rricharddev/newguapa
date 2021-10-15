3843b798f71f3794dc53dcd16dacb819da3fbc3a
Richardpeluca <https://github.com/rricharddev>
2021-10-15 11:15:24 -0300

    passergerwsgi

---
+++ cart/views.py
@@ -0,0 +1,70 @@
+import datetime
+import json
+from django.views import generic
+from django.db.models import Q
+from .utils import get_or_set_order_session
+from .models import Product
+from .forms import AddToCartForm
+from django.shortcuts import get_object_or_404, reverse, redirect
+from django.contrib import messages
+from django.conf import settings
+from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class ProductListView(generic.ListView):
+    template_name='cart/product_list.html'
+    queryset = Product.objects.all()
+
+
+class ProductDetailView(generic.FormView):
+    template_name = 'cart/product_detail.html'
+    form_class = AddToCartForm
+    
+    def get_object(self):
+        return get_object_or_404(Product, slug=self.kwargs["slug"])
+
+    def get_success_url(self):
+        return reverse("cart:summary")
+
+    def get_form_kwargs(self):
+        kwargs = super(ProductDetailView, self).get_form_kwargs()
+        kwargs["product_id"] = self.get_object().id
+        return kwargs
+
+    def form_valid(self, form):
+        order = get_or_set_order_session(self.request)
+        product = self.get_object()
+
+        item_filter = order.items.filter(
+            product=product,
+            colour=form.cleaned_data['colour'],
+            size=form.cleaned_data['size']
+        )
+
+        if item_filter.exists():
+            item = item_filter.first()
+            item.quantity += int(form.cleaned_data['quantity'])
+            item.save()
+
+        else:
+            new_item = form.save(commit=False)
+            new_item.product = product
+            new_item.order = order
+            new_item.save()
+        
+        return super(ProductDetailView, self).form_valid(form)
+
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        context['product'] = self.get_object()
+        return context
+
+
+        
+
+        
+
+    
+    
