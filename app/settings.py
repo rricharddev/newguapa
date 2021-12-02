4386d3e68187646149715e825d5510385c457b51
Richardpeluca <https://github.com/rricharddev>
2021-12-01 23:03:07 -0300

    settings

--- app/settings.py
+++ app/settings.py
@@ -150,7 +150,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'Tienda/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
