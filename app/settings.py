a500f494506bcec4d01d81a9e6bdeacbb232b493
Richardpeluca <https://github.com/rricharddev>
2022-03-20 22:40:59 -0300

    img

--- app/settings.py
+++ app/settings.py
@@ -165,7 +165,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'Tienda/static')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/productos')
 
 
 
